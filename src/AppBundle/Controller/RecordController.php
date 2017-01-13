697a00cc42695d19500dd078cd170d23f5582e5d
nikolaev <dmitriynik91@gmail.com>
2017-01-13 19:02:23 +0300

    Изучение symfony. Создание модели

---
+++ src/AppBundle/Controller/RecordController.php
@@ -0,0 +1,30 @@
+<?php
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\Record;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+
+class RecordController extends Controller
+{
+    /**
+     * @Route("/record/create")
+     */
+    public function createAction()
+    {
+        $product = new Record();
+        $product->setName('Dima');
+        $product->setPhone('89216576321');
+        $product->setDate(new \DateTime('2016-01-13'));
+        $product->setTime(new \DateTime('18:00'));
+
+        $em = $this->getDoctrine()->getManager();
+
+        $em->persist($product);
+        $em->flush();
+
+        return new Response('Saved new product with id ' . $product->getId());
+    }
+}
